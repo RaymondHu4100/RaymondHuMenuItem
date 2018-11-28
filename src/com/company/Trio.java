d78c27368918cc1514057b0412fb704b80e03594
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2018-11-28 08:40:19 -0500

    Initial commit

---
+++ src/com/company/Trio.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Trio implements MenuItem
+{
+    Sandwich sandwich;
+    Salad salad;
+    Drink drink;
+    public Trio(Sandwich sandwich, Salad salad, Drink drink)
+    {
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+    public String getName()
+    {
+        return sandwich.getName() + "/" + salad.getName() + "/" + drink.getName();
+    }
+    public double getPrice()
+    {
+        double Price = 0;
+        if (sandwich.getPrice() > salad.getPrice() && drink.getPrice() > salad.getPrice())
+        {
+            Price = sandwich.getPrice() + drink.getPrice();
+        }
+        if (salad.getPrice() > sandwich.getPrice() && drink.getPrice() > sandwich.getPrice())
+        {
+            Price = salad.getPrice() + drink.getPrice();
+        }
+        if (sandwich.getPrice() > drink.getPrice() && salad.getPrice() > drink.getPrice())
+        {
+            Price = sandwich.getPrice() + salad.getPrice();
+        }
+        return Price;
+    }
+}
